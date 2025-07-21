8a9a7a76ee062d9e713e3912c2042d765464dc74
hunter-volkman <huntervolkman@protonmail.com>
2025-07-21 10:30:50 -0400

    Fix React mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './app.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { FusionAuthProvider } from '@fusionauth/react-sdk';
+import { IS_AUTH_ENABLED } from './config/auth';
+import App from './App';
+import './app.css';
+import 'leaflet/dist/leaflet.css';
+
+const AuthProviderWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  if (!IS_AUTH_ENABLED) {
+    return <>{children}</>;
+  }
+
+  const clientId = import.meta.env.VITE_VIAM_OAUTH_CLIENT_ID;
+  const orgId = 'cc36ba4b-8053-441e-84fa-136270d34584';
+
+  if (!clientId) {
+    throw new Error("VITE_VIAM_OAUTH_CLIENT_ID is not set in your .env.local file.");
+  }
+
+  return (
+    <FusionAuthProvider
+      host="https://auth.viam.com"
+      clientID={clientId}
+      tenantId={orgId}
+      redirectURI={window.location.origin}
+    >
+      {children}
+    </FusionAuthProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProviderWrapper>
+        <App />
+      </AuthProviderWrapper>
+    </BrowserRouter>
+  </React.StrictMode>
+);
