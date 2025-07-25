038f5a7f04ecb8339944d7de032d99c84df3de1c
hunter-volkman <huntervolkman@protonmail.com>
2025-07-25 14:12:29 -0400

    Fix notify API

--- api/notify.ts
+++ api/notify.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
 import { kv } from '@vercel/kv';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import * as webpush from 'web-push';
@@ -37,28 +34,24 @@ export default async function handler(
     const subscriptions = await kv.smembers<webpush.PushSubscription[]>(subscriptionKey);
 
     if (subscriptions.length === 0) {
-      console.log(`[API Notify] No subscriptions found for store ${storeId}.`);
       return response.status(200).json({ success: true, message: 'No subscriptions to notify.' });
     }
-
-    console.log(`[API Notify] Found ${subscriptions.length} subscribers for store ${storeId}. Sending notifications...`);
 
     const notificationPayload = JSON.stringify({ title, message });
     const promises = subscriptions.map(subscription => 
       webpush.sendNotification(subscription, notificationPayload)
         .catch((error: any) => {
           if (error.statusCode === 410) {
-            console.log(`[API Notify] Subscription expired for ${subscription.endpoint}. Deleting.`);
+            console.log(`[API Notify] Subscription expired. Deleting.`);
             return kv.srem(subscriptionKey, subscription);
           } else {
-            console.error(`[API Notify] Failed to send notification to ${subscription.endpoint}:`, error.body);
+            console.error(`[API Notify] Failed to send notification:`, error.body);
           }
         })
     );
 
     await Promise.all(promises);
 
-    console.log('[API Notify] All notifications sent.');
     return response.status(200).json({ success: true });
 
   } catch (error) {
