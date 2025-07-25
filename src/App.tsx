28f3a0f538e908a5f4ea95d4c50999b21fef7c7b
hunter-volkman <huntervolkman@protonmail.com>
2025-07-25 14:14:07 -0400

    Fix root

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useStore } from './stores/store';
 import { monitor } from './services/monitor';
 import { push } from './services/push';
