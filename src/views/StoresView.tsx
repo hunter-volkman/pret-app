aa639a95988d9004ed5f983a1a9026993f7bfed7
hunter-volkman <huntervolkman@protonmail.com>
2025-07-28 13:36:38 -0400

    Fix stores view

--- src/views/StoresView.tsx
+++ src/views/StoresView.tsx
@@ -10,7 +10,7 @@ export function StoresView() {
   }
   
   return (
-    <div className="p-6 pb-28">
+    <div className="p-6">
       <div className="text-center mb-8">
         <h2 className="text-2xl font-bold text-gray-900 mb-2">Store Locations</h2>
         <p className="text-gray-600">Monitor and manage your store network</p>
