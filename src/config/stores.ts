89158dd7cee9b1541aa304bf4f2a82baf4d1589f
hunter-volkman <huntervolkman@protonmail.com>
2025-07-30 19:14:39 -0400

    Fix stores

--- src/config/stores.ts
+++ src/config/stores.ts
@@ -6,6 +6,7 @@ export interface Store {
   coords: { lat: number; lng: number }
   stockMachineId: string
   tempMachineId: string
+  tempPartId: string
   timezone: string
 }
 
@@ -17,6 +18,7 @@ export const STORES: Store[] = [
     coords: { lat: 40.7497832, lng: -73.9833294 },
     stockMachineId: 'a7c5717d-f48e-4ac8-b179-7c7aa73571de',
     tempMachineId: '948e0595-d307-425f-bd55-108e52046c2b',
+    tempPartId: '5fb0c449-9955-4963-9d1b-ab947cb05554',
     timezone: 'America/New_York'
   },
   {
@@ -26,6 +28,7 @@ export const STORES: Store[] = [
     coords: { lat: 40.737372, lng: -73.9931299 },
     stockMachineId: '467ae11d-077b-4a55-ad59-49c5a0fd7d1a',
     tempMachineId: '27971efd-baaa-4cb5-b5d0-2b5468daf0b4',
+    tempPartId: 'f4981097-a2fd-4998-91ae-20f2c9e9f5f1',
     timezone: 'America/New_York'
   },
   {
@@ -35,6 +38,7 @@ export const STORES: Store[] = [
     coords: { lat: 34.06352, lng: -118.4482214 },
     stockMachineId: '55fc689f-bb6d-4654-ba0e-273335bc7a42',
     tempMachineId: '4330c1e3-6a68-487c-8288-ce0124772bce',
+    tempPartId: 'bddd09bd-7c2d-4a45-98af-0c524b1a9959',
     timezone: 'America/Los_Angeles'
   },
   {
@@ -44,6 +48,7 @@ export const STORES: Store[] = [
     coords: { lat: 34.0574907, lng: -118.4209066 },
     stockMachineId: '1ceaf6ee-c926-4297-8fb6-3e702ed8e2eb',
     tempMachineId: 'fc6778ff-e3a0-4295-a52d-d92580fc2e81',
+    tempPartId: '92c416f8-b878-4eee-9725-a40926f5fd2c',
     timezone: 'America/Los_Angeles'
   },
   {
@@ -53,6 +58,7 @@ export const STORES: Store[] = [
     coords: { lat: 38.897684, lng: -77.0300862 },
     stockMachineId: '164a39ed-5a74-4786-9e71-c2f719fc3b22',
     tempMachineId: 'c65f15f3-21a9-46ef-9090-cd18c07178b5',
+    tempPartId: '3d178064-e159-4268-ab22-45689249dda6',
     timezone: 'America/New_York'
   },
   {
@@ -62,6 +68,7 @@ export const STORES: Store[] = [
     coords: { lat: 38.8975066, lng: -77.0067488 },
     stockMachineId: 'fc4be03e-3e05-4cef-8e4c-3b68ffbd627a',
     tempMachineId: 'de7e0050-24b3-442e-919a-a580a5e91213',
+    tempPartId: 'fd9b1333-92bf-417d-b93f-5e121e86d3cf',
     timezone: 'America/New_York'
   }
 ]
