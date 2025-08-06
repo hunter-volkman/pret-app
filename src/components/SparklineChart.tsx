8c5783660804a5613ef558a14c3e7f6b9e278282
hunter-volkman <huntervolkman@protonmail.com>
2025-08-06 16:08:39 -0400

    Fix sparkline chart

--- src/components/SparklineChart.tsx
+++ src/components/SparklineChart.tsx
@@ -2,37 +2,37 @@ import Chart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
 
 interface SparklineChartProps {
-  data: { x: any; y: any }[];
-  color: string;
+  data: { x: any; y: any }[];
+  color: string;
 }
 
 export function SparklineChart({ data, color }: SparklineChartProps) {
-  const options: ApexOptions = {
-    chart: {
-      type: 'line',
-      width: 80, // Reduced from 100
-      height: 35,
-      sparkline: {
-        enabled: true,
-      },
-    },
-    stroke: {
-      curve: 'smooth',
-      width: 2,
-    },
-    colors: [color],
-    tooltip: {
-      enabled: false,
-    },
-    xaxis: {
-      type: 'datetime',
-    },
-  };
+  const options: ApexOptions = {
+    chart: {
+      type: 'line',
+      width: 60, // Reduced from 80
+      height: 35,
+      sparkline: {
+        enabled: true,
+      },
+    },
+    stroke: {
+      curve: 'smooth',
+      width: 2,
+    },
+    colors: [color],
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      type: 'datetime',
+    },
+  };
 
-  const series = [{
-    name: 'Trend',
-    data: data,
-  }];
+  const series = [{
+    name: 'Trend',
+    data: data,
+  }];
 
-  return <Chart options={options} series={series} type="line" height={35} width={80} />;
+  return <Chart options={options} series={series} type="line" height={35} width={60} />;
 }
